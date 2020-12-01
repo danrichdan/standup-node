afd67dffb174fcba58acd7b6d32ae38c0c21e1d2
Dan Riches <driches@ymail.com>
2020-11-30 21:15:42 -0800

    fixed teh about section

--- routes/main.js
+++ routes/main.js
@@ -4,8 +4,6 @@ const mainController = require("../controllers/main");
 
 const router = express.Router();
 
-router.get("/about", mainController.getAbout);
-
 router.get("/dashboard", mainController.getDashboard);
 
 router.get("/", mainController.getIndex);
