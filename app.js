75f9e90e2e31582851fd41ebcdda73af98f42b85
Dan Riches <driches@ymail.com>
2020-12-12 13:52:15 -0800

    add field validation

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use(mainRoute);
 app.use(errorsController.get404);
 
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
     app.listen(process.env.PORT || 3000);
   })
